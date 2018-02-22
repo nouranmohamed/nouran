dd2a5294148ff2cb38150aa85308c6c896e07e7b
nouranmohamed <36602978+nouranmohamed@users.noreply.github.com>
2018-02-22 13:09:15 +0200

    Add files via upload

---
+++ Turn_on_Led_by_a_button.ino
@@ -0,0 +1,27 @@
+
+//pin1:iput for button
+//pin2:output for lead
+int buttonvalue=0;
+int button=1;
+int led=2;
+
+void setup() {
+  pinMode(button,INPUT);
+  
+  pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  buttonvalue=digitalRead(button);
+  if(buttonvalue != 0){
+    // if button is pressed then led on
+      digitalWrite(led,HIGH);
+  }
+  else
+  {
+      //if button is not pressed then lead off
+      digitalWrite(led,LOW);
+  }
+    
+}
